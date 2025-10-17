266cc34b2f77d673a4507ade6aa1a087aea33a37
DIOGO VINÍCIUS ABREU DAMASCENO <diogovini2007@gmail.com>
2025-10-17 14:22:58 -0300

    Adicionando comentarios

--- src/P02.java
+++ src/P02.java
@@ -2,12 +2,13 @@ import java.util.Scanner;
 
 public class P02 {
     public static void main(String[] args) {
-
+        //declarando variaveis
         int num1, num2, num3, num4, soma;
         int multiplicador;
         double media;
+        //criando scanner
         Scanner sc = new Scanner(System.in);
-
+        //lendo entrada do teclado
         System.out.println("Digite o primeiro numero: ");
         num1 = sc.nextInt();
 
@@ -19,17 +20,21 @@ public class P02 {
 
         System.out.println("Digite o quarto numero: ");
         num4 = sc.nextInt();
-
+        //processando valores:
+        //soma
         soma = num1 + num2 + num3 + num4;
+        //multiplicacao
         multiplicador = (num1 + num2) * (num3 + num4);
+        //media
         media = (num1 + num2 + num3 + num4) / 4;
+        //saida dos valores:
         System.out.println("A soma dos numeros " + num1 + " e " + num2 + " e " + num3 + " equivale a: " + soma);
         System.out.println("A multiplicaçao dos numeros " + num1 + " + " + num2 + " vezes os numeros "
                 + num3 + " + " + num4 + "equivale a: " + multiplicador);
         System.out.println("A media dos valores " + num1 + " e " + num2 + " e " + num3 + " e " + num4 + " equivale a " + media);
 
 /*
- tem que usar parenteses para definir a ordem que voce quer que seja feita antes da ordem comum
+ Ordem em que os numeros sao calculados:
  1º	Parênteses	()	Sempre primeiro
  2º	Expoentes	 ^, ∗∗,  ​	Após Parênteses
  3º	Multiplicação e Divisão	∗,×,/,÷	Da esquerda para a direita
